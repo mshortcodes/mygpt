df97a93d65aaab63108907c1edc4f3a8c88f04dd
Michael Short <mshort0403@gmail.com>
2024-03-06 16:44:47 -0800

    Make cards

---
+++ src/cards.js
@@ -0,0 +1,63 @@
+export const cards = {
+	card1: [
+		{
+			h4: 'Write a text message',
+			p: 'asking a friend to be my plus-one at my wedding',
+		},
+		{ h4: 'Plan a trip', p: 'to see the northern lights in Norway' },
+		{ h4: 'Write a thank-you note', p: 'to my interviewer' },
+		{ h4: 'Give me ideas', p: `for what to do with my kid's art` },
+		{
+			h4: 'Suggest fun activities',
+			p: 'for a family of 4 to do indoors on a rainy day',
+		},
+	],
+
+	card2: [
+		{
+			h4: 'Help me study',
+			p: 'vocabulary for a college entrance exam',
+		},
+		{
+			h4: 'Brainstorm names',
+			p: `for an orange cat we're adopting from the shelter`,
+		},
+		{ h4: 'Tell me a fun fact', p: 'about the Roman Empire' },
+		{ h4: 'Create a content calendar', p: 'for a Tiktok account' },
+		{
+			h4: 'Create a personal webpage for me',
+			p: 'after asking me three questions',
+		},
+	],
+
+	card3: [
+		{
+			h4: 'Help me pick',
+			p: 'a birthday gift for my mom who likes gardening',
+		},
+		{
+			h4: 'Suggest fun activities',
+			p: 'for a family visiting San Francisco',
+		},
+		{
+			h4: 'Make a content strategy',
+			p: 'for a newsletter featuring free local weekend events',
+		},
+		{ h4: 'Help me pick', p: 'an outfit that will look good on camera' },
+		{
+			h4: 'Write a course overview',
+			p: 'on the psychology behind decision-making',
+		},
+	],
+
+	card4: [
+		{
+			h4: 'Write a Python script',
+			p: 'to automate sending daily email reports',
+		},
+		{ h4: 'Help me pick', p: 'a gift for my dad who loves fishing' },
+		{ h4: 'Come up with concepts', p: 'for a retro-style arcade game' },
+		{ h4: 'Plan a trip', p: 'to see the best of New York in 3 days' },
+		{ h4: 'Explain nostalgia', p: 'to a kindergartener' },
+	],
+};
