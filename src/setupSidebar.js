81a1731c4565c869525f401c51e6d6fc039bf0b3
Michael Short <mshort0403@gmail.com>
2024-03-09 15:33:11 -0800

    Update svg

--- src/setupSidebar.js
+++ src/setupSidebar.js
@@ -3,25 +3,25 @@ export const setupSidebar = () => {
 	const closeSidebarBtn = document.getElementById('closeSidebarBtn');
 	const sidebarBtnDiv = document.getElementById('sidebarBtnDiv');
 	const sidebar = document.querySelector('aside');
-	const newChatBtn = document.getElementById('newChatBtn');
+	const clearChatBtn = document.getElementById('clearChatBtn');
 
-	// show sidebar, switch chevron, adjust positioning, hide new chat button
+	// show sidebar, switch chevron, adjust positioning, hide clear chat button
 	openSidebarBtn.addEventListener('click', () => {
 		sidebar.classList.add('md:flex');
 		openSidebarBtn.classList.add('hidden');
 		closeSidebarBtn.classList.remove('hidden');
 		sidebarBtnDiv.classList.remove('left-0');
 		sidebarBtnDiv.classList.add('left-80');
-		newChatBtn.classList.add('md:hidden');
+		clearChatBtn.classList.add('md:hidden');
 	});
 
-	// hide sidebar, switch chevron, adjust positioning, show new chat button
+	// hide sidebar, switch chevron, adjust positioning, show clear chat button
 	closeSidebarBtn.addEventListener('click', () => {
 		sidebar.classList.remove('md:flex');
 		openSidebarBtn.classList.remove('hidden');
 		closeSidebarBtn.classList.add('hidden');
 		sidebarBtnDiv.classList.remove('left-80');
 		sidebarBtnDiv.classList.add('left-0');
-		newChatBtn.classList.remove('md:hidden');
+		clearChatBtn.classList.remove('md:hidden');
 	});
 };
